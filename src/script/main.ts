c08c4ca139dfaba5b3f122c13c7534cbc86ea532
TheRedVillager <zollner.michael@outlook.de>
2023-03-10 21:13:57 +0100

    removed demo cube

--- src/script/main.ts
+++ src/script/main.ts
@@ -14,29 +14,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true
-});
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+
+
+  render();
+}
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   render();
 }
 
